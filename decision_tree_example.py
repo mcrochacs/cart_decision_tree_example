103e141a0c812644d39a953f3d3daa686e917dc8
Marcus Rocha <mcrocha.cs@gmail.com>
2019-04-29 19:58:54 -0300

    Implementei a primeira parte do código

--- decision_tree_example.py
+++ decision_tree_example.py
@@ -1,1 +1,14 @@
-# Exemplo de Arvore de Decisao
+# Decision Tree Classifier
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+
+# Load the Iris Datasets
+dataset = datasets.load_iris()
+
+# Fit a CART model to the data
+model = DecisionTreeClassifier()
+model.fit(dataset.data, dataset.target)
+print(model)
+
+# Make Predictions...
