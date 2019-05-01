b93443e452b0e38dda506b26f3a0fa1eb0ac8990
Marcus Rocha <mcrocha.cs@gmail.com>
2019-04-30 21:14:27 -0300

    Estudando o código

--- decision_tree_example.py
+++ decision_tree_example.py
@@ -1,19 +1,42 @@
 # Decision Tree Classifier
+import pandas
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier
 
-# Load the Iris Datasets
-dataset = datasets.load_iris()
+# Load the Iris Datasets 1
+dataset1 = datasets.load_iris()
+print("<<<<< First Dataset - without pandas >>>>>")
+print(dataset1.data)
+print(dataset1.target)
+print(" ")
+
+
+# Load the Iris Datasets 2
+dataset2 = pandas.read_csv("iris_data.csv")
+print("<<<<< Second Dataset - with pandas >>>>>")
+print(dataset2.shape)
+print(dataset2.head(20))
+print(dataset2.describe())
+print(" ")
+
 
 # Fit a CART model to the data
+# model = classificador
 model = DecisionTreeClassifier()
-model.fit(dataset.data, dataset.target)
+
+
+# ajusta o modelo sobre os dados
+# dataset.data = dados de treinamento / dataset.target = alvo de treinamento
+model.fit(dataset1.data, dataset1.target)
 print(model)
+print(" ")
+
 
 # Make Predictions
-expected = dataset.target
-predicted = model.predict(dataset.data)
+expected = dataset1.target
+predicted = model.predict(dataset1.data)
+
 
 # Summarize the fit of the model
 print(metrics.classification_report(expected, predicted))
